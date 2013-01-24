c0b9b411a5e55f491c1430955d7ce77ffa7cfc62
Samuel Rounce <me@samuelrounce.co.uk>
2013-01-24 17:27:46 +0000

    Setup project and added three.js@r55 submodule dependency

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-define([ './treecle' ], function( Treecle ) {
+requirejs.config({
+  shim : {
+    '/js/lib/three.js/build/three.js': {
+      exports: 'three',
+      init: function () {
+        console.log("THREE, u thar brah? ", THREE);
+        return THREE;
+      }
+    }
+  }
+});
 
-var app = new Treecle();
+
+define([ 'lib/domReady', 'treecle' ], function( domReady, Treecle ) {
+
+domReady(function() {
+  var app = new Treecle();
+})
 
 })
--- js/treecle.js
+++ js/treecle.js
@@ -1,16 +1,28 @@
-define([], function(){
+define(['/js/lib/three.js/build/three.js'], function(THREE){
 
 return function TreecleApp()
 {
 
   function TreecleApp()
   {
+//  this.renderer =
 
+    initUpdateLoop.call(this);
   }
 
   TreecleApp.prototype.tick = function tick()
   {
+    requestAnimationFrame(this.render); 
+  }
 
+  TreecleApp.prototype.render = function render()
+  {
+    
+  }
+
+  function initUpdateLoop()
+  {
+    this.tick(); 
   }
 
   return new TreecleApp();
