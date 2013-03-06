ef7e88d36d8c39cfb3ba34add3f3c4b0f5b067df
Samuel Rounce <me@samuelrounce.co.uk>
2013-03-06 16:33:55 +0000

    Got the Leapmotion finally working, no fault of my own of course! (sic)

--- src/treecle.js
+++ src/treecle.js
@@ -34,7 +34,7 @@ return function TreecleApp( structDef )
     if ( !Detector.webgl ) Detector.addGetWebGLMessage();
 
     _camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 10000);
-    _camera.position.set(100, 100, -250);
+    _camera.position.set(100, 100, -120);
     _camera.lookAt( _camTarget );
 
     EventManager.setCamera( _camera );
@@ -67,16 +67,16 @@ return function TreecleApp( structDef )
       _sceneRotX = frameTrans[0] * radian;
 
       _sceneCurY = Util.map(frameTrans[1], -300, 300, 0, 180);
-      _sceneRotY = -(_sceneCurY) * radian
+      _sceneRotY = -(_sceneCurY) * radian;
 
-      _lmRotate.x = _camTarget.x + camRadius * Math.sin(_sceneRotY) * Math.cos(_sceneRotX) * _lmVelocityScale;
-      _lmRotate.y = _camTarget.y + camRadius * Math.sin(_sceneRotY) * Math.sin(_sceneRotX) * _lmVelocityScale;
-      _lmRotate.z = _camTarget.z + camRadius * Math.cos(_sceneRotY) * _lmVelocityScale;
+      _lmRotate.x = _camTarget.x * _lmVelocityScale + camRadius * Math.sin(_sceneRotY) * Math.cos(_sceneRotX);
+      _lmRotate.z = _camTarget.y * _lmVelocityScale + camRadius * Math.sin(_sceneRotY) * Math.sin(_sceneRotX);
+      _lmRotate.y = _camTarget.z * _lmVelocityScale + camRadius * Math.cos(_sceneRotY);
 
       _camera.position.set( _lmRotate.x, _lmRotate.y, _lmRotate.z );
     }
 
-    _camera.updateProjectionMatrix();
+    //_camera.updateProjectionMatrix();
     _camera.lookAt(_camTarget);
 
     this.render.call(this, frame);
