836e6e7f56b58b019a12e598971ff69b5f4c0f64
Samuel Rounce <me@samuelrounce.co.uk>
2013-03-07 14:19:06 +0000

    Added a couple of extra icon images Fixed alpha bug in NodeSpriteTexture Doubled size of NodeSpriteTexture framebuffer

--- src/nodespritetexture.js
+++ src/nodespritetexture.js
@@ -10,8 +10,8 @@ return function NodeSpriteTexture( title, imageURL )
     , _ctx
     , _texture
 
-    , _width = 512
-    , _height = 512
+    , _width = 1024
+    , _height = 1024
 
     , _title = ""
     , _imageURL = "/assets/misc/coolface.png"
@@ -21,6 +21,9 @@ return function NodeSpriteTexture( title, imageURL )
     _canvas = document.createElement('canvas');
     _canvas.width = _width;
     _canvas.height = _height;
+
+//if(document.querySelectorAll('canvas').length === 1)
+  //document.body.appendChild(_canvas)
 
     _title = title || _title;
     _imageURL = imageURL || _imageURL; 
@@ -41,7 +44,7 @@ return function NodeSpriteTexture( title, imageURL )
   {
     var centerX = _width / 2
       , centerY = _height / 2
-      , radius = 96
+      , radius = centerX / 1.5
       , angle
       , gradient = _ctx.createLinearGradient(0, centerY/2, 0, _height);
 
@@ -59,18 +62,13 @@ return function NodeSpriteTexture( title, imageURL )
       _ctx.beginPath();
       drawPolygon(_ctx, 6, radius, centerX, centerY);
       _ctx.clip();
-      _ctx.fill();
-      _ctx.drawImage(img, centerX/2, centerY/2, _width - centerX, _height - centerY);
       _ctx.closePath();
+      _ctx.fill();
+
+      _ctx.drawImage(img, centerX/2, centerY/2, _width - centerX, _height - centerY);
       _texture.needsUpdate = true;
     }
     img.src = _imageURL;
-
-//  _ctx.moveTo(0+5, 0+5);
-//  _ctx.lineTo(centerX, _height-5);
-//  _ctx.lineTo(_width-5, 0+5);
-
-//  _ctx.restore();
 
     _ctx.beginPath();
     drawPolygon(_ctx, 6, radius, centerX, centerY);
@@ -80,36 +78,57 @@ return function NodeSpriteTexture( title, imageURL )
     _ctx.stroke();
 
     _ctx.moveTo(centerX, 5)
-    _ctx.fillStyle = '#FFFFFF';
+    _ctx.fillStyle = '#FFFF2F';
     _ctx.textAlign = 'right';
     _ctx.font = 'bold 36px Helvetica Neue, Calibri, sans-serif';
     //_ctx.fillText(_title, centerX, 192)
-    drawText(_ctx, _title, centerX, centerX - 116, 146)
+    drawText(_ctx, _title, (centerX - (centerX/3) - 24), centerX - 116, 146);
+    _ctx.fillStyle = 'transparent';
   }
 
   function drawText( ctx, text, maxWidth, x, y )
   {
-    var outBuf = ""
-      , _text = text || ""
+    var _text = text || ""
       , _maxWidth = maxWidth || ctx.width + x
       , _x = x || 0
       , _y = y || 0
       , wordList = text.split(/[\s\r]/g)
       , _lineHeight = parseInt( parseInt(ctx.font) * 1.2 )
+      , _align = ctx.textAlign
+      , _isAlignRight = ['right', 'end'].indexOf(_align) > -1
 
     wordList.forEach(function( word, index ) {
       var doWrap
+        , wordWidth = ctx.measureText(word).width;
 
-      _x += ctx.measureText(word).width
-      doWrap = _x > _maxWidth
+      doWrap = (_x + wordWidth > _maxWidth)
+            || ( (_x + wordWidth < x) && _isAlignRight );
 
       if( doWrap ) {
-        _x = x
-        _y += _lineHeight
+        switch( ctx.textAlign ) {
+          case 'right' :
+            _x = maxWidth;
+            break;
+
+          case 'end' :
+            _x = maxWidth;
+            break;
+
+          default :
+            _x = x;
+            break;
+        }
+
+        _y += _lineHeight;
+      } else {
+        if( _isAlignRight )
+          _x -= wordWidth
+        else
+          _x += wordWidth
       }
 
-      ctx.fillText( word, _x, _y )
-    })
+      ctx.fillText( word, _x, _y );
+    });
   }
 
   function drawPolygon( ctx, sides, radius, centerX, centerY )
